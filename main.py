dce10ce3497b2a686d162ba1c218b51092114c5f
Enes Türk <partuminfernum@gmail.com>
2024-03-21 20:49:40 +0300

    automatization for queries

---
+++ main.py
@@ -0,0 +1,46 @@
+import psycopg2
+import pandas as pd
+
+# Read SQL file and split queries
+def read_sql_file(file_path):
+    with open(file_path, 'r') as file:
+        sql_queries = file.read().split(';')
+    # Remove empty queries
+    sql_queries = [query.strip() for query in sql_queries if query.strip()]
+    return sql_queries
+
+# Connect to PostgreSQL database
+def connect_to_database():
+    try:
+        connection = psycopg2.connect(
+            user='postgres',
+            password="1482002",
+            host="localhost",
+            port="5432",
+            database="sql_project"
+        )
+        return connection
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+        return None
+
+# Execute queries and return results as Pandas DataFrames
+def execute_queries(connection, queries):
+    dataframes = []
+    cursor = connection.cursor()
+    try:
+        for query in queries:
+            cursor.execute(query)
+            # Fetch all rows from the result set
+            rows = cursor.fetchall()
+            # Convert fetched data into DataFrame
+            df = pd.DataFrame(rows, columns=[desc[0] for desc in cursor.description])
+            dataframes.append(df)
+        print("All queries executed successfully!")
+        return dataframes
+    except (Exception, psycopg2.Error) as error:
+        print("Error executing SQL query:", error)
+        return None
+    finally:
+        cursor.close()
+
--- queries/1_job_comparison.sql
+++ queries/1_job_comparison.sql
@@ -1,3 +1,4 @@
+-- data role salaries
 WITH jobs AS (
     SELECT
         job_id,
@@ -11,6 +12,85 @@ WITH jobs AS (
     FROM job_postings_fact
     WHERE salary_year_avg IS NOT NULL
 )
+SELECT
+    job_title_short,
+    COUNT(job_id) AS job_count,
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary
+FROM jobs
+GROUP BY
+    job_title_short;
 
-SELECT *
-FROM jobs;
+-- skill demand for roles
+-- data engineer role
+WITH remote_jobs AS (
+    SELECT job_id
+    FROM job_postings_fact
+    WHERE job_work_from_home = true 
+        AND job_title_short = 'Data Engineer'
+)
+
+SELECT 
+    sjd.skill_id,
+    COUNT(rj.job_id) AS job_count,
+    sd.skills AS skill_name
+FROM 
+    skills_job_dim AS sjd
+JOIN 
+    remote_jobs AS rj ON rj.job_id = sjd.job_id
+LEFT JOIN 
+    skills_dim AS sd ON sd.skill_id = sjd.skill_id
+GROUP BY 
+    sjd.skill_id, sd.skills
+ORDER BY 
+    job_count DESC
+LIMIT 15;
+
+-- data scientist role
+WITH remote_jobs AS (
+    SELECT job_id
+    FROM job_postings_fact
+    WHERE job_work_from_home = true 
+        AND job_title_short = 'Data Scientist'
+)
+
+SELECT 
+    sjd.skill_id,
+    COUNT(rj.job_id) AS job_count,
+    sd.skills AS skill_name
+FROM 
+    skills_job_dim AS sjd
+JOIN 
+    remote_jobs AS rj ON rj.job_id = sjd.job_id
+LEFT JOIN 
+    skills_dim AS sd ON sd.skill_id = sjd.skill_id
+GROUP BY 
+    sjd.skill_id, sd.skills
+ORDER BY 
+    job_count DESC
+LIMIT 15;
+
+-- data analyst role
+WITH remote_jobs AS (
+    SELECT job_id
+    FROM job_postings_fact
+    WHERE job_work_from_home = true 
+        AND job_title_short = 'Data Analyst'
+)
+
+SELECT
+    sjd.skill_id,
+    COUNT(rj.job_id) AS job_count,
+    sd.skills AS skill_name
+FROM 
+    skills_job_dim AS sjd
+JOIN 
+    remote_jobs AS rj ON rj.job_id = sjd.job_id
+LEFT JOIN 
+    skills_dim AS sd ON sd.skill_id = sjd.skill_id
+WHERE
+    sjd.skill_id <> 186
+GROUP BY 
+    sjd.skill_id, sd.skills
+ORDER BY 
+    job_count DESC
+LIMIT 15;
